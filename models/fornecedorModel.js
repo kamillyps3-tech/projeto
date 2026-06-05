8c6f06369d8bac4713cf7bc561d561747beed111
Kamilly Pereira <kamillyps3@gmail.com>
2026-06-05 02:23:53 -0300

    Update fornecedorModel.js

--- models/fornecedorModel.js
+++ models/fornecedorModel.js
@@ -1,8 +1,8 @@
 const connection = require ("../connection/connection")
 
-class fornecedorModel{
+class FornecedorModel{
     listar(){
-        const sql = "select * from fornecedor"
+        const sql = "SELECT * FROM fornecedores"
 
         return new Promise ((resolve, reject) => {
           connection.query(sql, {}, (error, resposta) => {
@@ -19,48 +19,54 @@ class fornecedorModel{
         })
     }
     criar(novoFornecedor){
-        const sql = "insert into fornecedor set ?"
+        const sql = "INSERT INTO fornecedores SET ?"
         console.log("Dados do fornecedor a serem inseridos: ", novoFornecedor)
 
         return new Promise ((resolve, reject) =>{
           connection.query(sql, novoFornecedor, (error, resposta) =>{
             if(error){
-              console.log("Erro ao inserir fornecedor")
+              console.log("Erro ao inserir fornecedor.")
               console.log(error.message)
+              return
               reject(error)
-            }
+            } 
+              console.log("Fornecedor inserido com sucesso.")
+              resolve(resposta)
+            
           })
         })
     }
-    atualizar(fornecedorAtualizado, id){
-      const sql = "update fornecedor set ? where id=?"
-      return new Promise ((resolve, reject ) => {
-         connection.query(sql, [fornecedorAtualizado, id],(error, resposta) =>{
-          if(error){
-            console.log("Erro ao atualizar fornecedor")
-            console.log(error.message)
-            reject(error)
-          }
-          console.log("Fornecedor atualizado com sucesso.")
-         }) 
-      })
-    }
-    deletar(fornecedorExclusao, id){
-      const sql = "delete from fornecedor where id=?"
-      return new Promise ((resolve, reject) => {
-          connection.query(sql, [fornecedorExclusao, id], (error, resposta) =>{
-            if(error){
-              console.log("Erro ao deletar o fornecedor")
-              console.log(error.message)
-              reject(error)
-            }
-            console.log("Fornecedor deletado com sucesso")
-            resolve(resposta)
-          })
-      })
-    }
+  atualizar(fornecedorAtualizado, id){
+  const sql = "UPDATE fornecedores SET ? WHERE id=?"
+
+  return new Promise((resolve, reject) => {
+    connection.query(sql, [fornecedorAtualizado, id], (error, resposta) => {
+      if(error){
+        return reject(error)
+      }
+
+      console.log("Fornecedor atualizado com sucesso.")
+      resolve(resposta)
+    })
+  })
+}
+deletar(id){
+  const sql = "DELETE FROM fornecedores WHERE id=?"
+
+  return new Promise((resolve, reject) => {
+    connection.query(sql, [id], (error, resposta) => {
+      if(error){
+        console.log("Erro ao deletar o fornecedor.")
+        console.log(error.message)
+        return reject(error)
+      }
+      console.log("Fornecedor deletado com sucesso.")
+      resolve(resposta)
+    })
+  })
+}
 }
 
-module.exports = new fornecedorModel()
+module.exports = new FornecedorModel()
 
 
