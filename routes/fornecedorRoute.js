d81aadd02ea3dd1b6cfd95ddf072e7c70a19e9ed
Kamilly Pereira <kamillyps3@gmail.com>
2026-06-10 00:37:03 -0300

    Update fornecedorRoute.js

--- routes/fornecedorRoute.js
+++ routes/fornecedorRoute.js
@@ -1,40 +1,52 @@
-const fornecedorController = require("../controllers/fornecedorController")
+const Router = require("express").Router;
 
-const Router = require("express").Router
+const router = Router();
 
-const router = Router()
+const fornecedorController = require("../controllers/fornecedorController");
+
+// CRUD
 
 router.get("/fornecedores", (req, res) => {
-    fornecedorController.buscar()
+    const respController = fornecedorController.buscar();
+
+    respController
         .then(fornecedores => res.status(200).json(fornecedores))
-        .catch(error => res.status(400).json(error.message))
-})
+        .catch(error => res.status(400).json(error.message));
+});
 
 router.post("/fornecedores", (req, res) => {
-    const novoFornecedor = req.query
+    const novoFornecedor = req.body;
 
-    fornecedorController.criar(novoFornecedor)
-        .then(resultado => res.status(201).json(resultado))
-        .catch(error => res.status(400).json(error.message))
-})
+    const fornecedor = fornecedorController.criar(novoFornecedor);
 
-router.put("/fornecedores", (req, res) => {
-    const id = req.query.id
+    fornecedor
+        .then(fornecedorInserido => res.status(201).json(fornecedorInserido))
+        .catch(error => res.status(400).json(error.message));
+});
 
-    const fornecedorAtualizado = { ...req.query }
-    delete fornecedorAtualizado.id
+router.put("/fornecedor/:id", (req, res) => {
+    const { id } = req.params;
 
-    fornecedorController.alterar(fornecedorAtualizado, id)
-        .then(resultado => res.status(200).json(resultado))
-        .catch(error => res.status(400).json(error.message))
-})
+    const fornecedorAtualizado = req.body;
 
-router.delete("/fornecedores", (req, res) => {
-    const id = req.query.id
+    const fornecedorAtualizar = fornecedorController.alterar(
+        fornecedorAtualizado,
+        id
+    );
 
-    fornecedorController.apagar(id)
-        .then(resultado => res.status(200).json(resultado))
-        .catch(error => res.status(400).json(error.message))
-})
+    fornecedorAtualizar
+        .then(resultAtualizado => res.status(200).json(resultAtualizado))
+        .catch(error => res.status(400).json(error.message));
+});
 
-module.exports = router
+router.delete("/fornecedor/:id", (req, res) => {
+    const { id } = req.params;
+
+    const fornecedorDeletar = fornecedorController.apagar(id);
+
+    fornecedorDeletar
+        .then(fornecedorDeletado => res.status(200).json(fornecedorDeletado))
+        .catch(error => res.status(400).json(error.message));
+});
+
+module.exports = router;
