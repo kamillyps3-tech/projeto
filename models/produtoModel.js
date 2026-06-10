0b9800e12652363c41723a4aa93d0cc12ae53ed8
Kamilly Pereira <kamillyps3@gmail.com>
2026-06-10 00:36:06 -0300

    Update produtoModel.js

--- models/produtoModel.js
+++ models/produtoModel.js
@@ -1,68 +1,83 @@
 const connection = require("../connection/connection");
 
-class produtoModel {
-  listar() {
-    const sql = "SELECT * FROM produtos";
+class ProdutoModel {
 
-    return new Promise((resolve, reject) => {
-      connection.query(sql, {}, (error, resposta) => {
-        if (error) {
-          console.log("Ocorreu um erro na consulta.");
-          reject(error);
-        }
+    listar() {
+        const sql = "SELECT * FROM produtos";
 
-        console.log("Consulta realizada com sucesso.");
-        resolve(resposta);
-      });
-    });
-  }
-  criar(novoProduto) {
-    const sql = "INSERT INTO produtos SET ?";
-    console.log("Dados do cliente a serem inseridos: ", novoProduto);
+        return new Promise((resolve, reject) => {
+            connection.query(sql, (error, resposta) => {
 
-    return new Promise((resolve, reject) => {
-      connection.query(sql, novoProduto, (error, resposta) => {
-        if (error) {
-          console.log("Erro ao inserir produto.");
-          console.log(error.message);
-          return;
-          reject(error);
-        }
-        console.log("Produto inserido com sucesso.");
-        resolve(resposta);
-      });
-    });
-  }
-  atualizar(produtoAtualizado, id) {
-    const sql = "UPDATE produtos SET ? WHERE id=?";
+                if (error) {
+                    console.log("Ocorreu um erro na consulta.");
+                    reject(error);
+                    return;
+                }
 
-    return new Promise((resolve, reject) => {
-      connection.query(sql, [produtoAtualizado, id], (error, resposta) => {
-        if (error) {
-          return reject(error);
-        }
+                console.log("Consulta realizada com sucesso.");
+                resolve(resposta);
+            });
+        });
+    }
 
-        console.log("Produto atualizado com sucesso.");
-        resolve(resposta);
-      });
-    });
-  }
-  deletar(id) {
-    const sql = "DELETE FROM produtos WHERE id=?";
+    criar(novoProduto) {
+        const sql = "INSERT INTO produtos SET ?";
 
-    return new Promise((resolve, reject) => {
-      connection.query(sql, [id], (error, resposta) => {
-        if (error) {
-          console.log("Erro ao deletar o produto.");
-          console.log(error.message);
-          return reject(error);
-        }
-        console.log("Produto deletado com sucesso.");
-        resolve(resposta);
-      });
-    });
-  }
+        console.log("Dados do produto a serem inseridos:", novoProduto);
+
+        return new Promise((resolve, reject) => {
+            connection.query(sql, novoProduto, (error, resposta) => {
+
+                if (error) {
+                    console.log("Erro ao inserir produto");
+                    console.log(error.message);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Produto inserido com sucesso.");
+                resolve(resposta);
+            });
+        });
+    }
+
+    atualizar(produtoAtualizado, id) {
+        const sql = "UPDATE produtos SET ? WHERE id = ?";
+
+        return new Promise((resolve, reject) => {
+            connection.query(sql, [produtoAtualizado, id], (error, resposta) => {
+
+                if (error) {
+                    console.log("Erro ao atualizar produto");
+                    console.log(error.message);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Produto atualizado com sucesso.");
+                resolve(resposta);
+            });
+        });
+    }
+
+    deletar(id) {
+        const sql = "DELETE FROM produtos WHERE id = ?";
+
+        return new Promise((resolve, reject) => {
+            connection.query(sql, [id], (error, resposta) => {
+
+                if (error) {
+                    console.log("Erro ao deletar o produto");
+                    console.log(error.message);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Produto deletado com sucesso.");
+                resolve(resposta);
+            });
+        });
+    }
 }
 
-module.exports = new produtoModel();
-
+module.exports = new ProdutoModel();
