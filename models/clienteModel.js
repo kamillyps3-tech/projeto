3cddf04e5d8dfabc470300a54fa9264b469ce888
Kamilly Pereira <kamillyps3@gmail.com>
2026-06-10 00:35:20 -0300

    Update clienteModel.js

--- models/clienteModel.js
+++ models/clienteModel.js
@@ -1,70 +1,83 @@
-const connection = require ("../connection/connection")
+const connection = require("../connection/connection");
 
-class clienteModel{
-    listar(){
-        const sql = "SELECT * FROM clientes"
+class ClienteModel {
 
-        return new Promise ((resolve, reject) => {
-          connection.query(sql, {}, (error, resposta) => {
-      
-            if(error){
-                console.log("Ocorreu um erro na consulta.")
-                reject(error)
-            }
-            
-            console.log("Consulta realizada com sucesso.")
-            resolve(resposta)    
+    listar() {
+        const sql = "SELECT * FROM clientes";
 
-          })
-        })
+        return new Promise((resolve, reject) => {
+            connection.query(sql, (error, resposta) => {
+
+                if (error) {
+                    console.log("Ocorreu um erro na consulta.");
+                    reject(error);
+                    return;
+                }
+
+                console.log("Consulta realizada com sucesso.");
+                resolve(resposta);
+            });
+        });
     }
-    criar(novoCliente){
-        const sql = "INSERT INTO clientes SET ?"
-        console.log("Dados do cliente a serem inseridos: ", novoCliente)
 
-        return new Promise ((resolve, reject) =>{
-          connection.query(sql, novoCliente, (error, resposta) =>{
-            if(error){
-              console.log("Erro ao inserir cliente.")
-              console.log(error.message)
-              return
-              reject(error)
-            } 
-              console.log("Cliente inserido com sucesso.")
-              resolve(resposta)
-            
-          })
-        })
+    criar(novoCliente) {
+        const sql = "INSERT INTO clientes SET ?";
+
+        console.log("Dados do cliente a serem inseridos:", novoCliente);
+
+        return new Promise((resolve, reject) => {
+            connection.query(sql, novoCliente, (error, resposta) => {
+
+                if (error) {
+                    console.log("Erro ao inserir cliente");
+                    console.log(error.message);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Cliente inserido com sucesso.");
+                resolve(resposta);
+            });
+        });
     }
-  atualizar(clienteAtualizado, id){
-  const sql = "UPDATE clientes SET ? WHERE id=?"
 
-  return new Promise((resolve, reject) => {
-    connection.query(sql, [clienteAtualizado, id], (error, resposta) => {
-      if(error){
-        return reject(error)
-      }
+    atualizar(clienteAtualizado, id) {
+        const sql = "UPDATE clientes SET ? WHERE id = ?";
 
-      console.log("Cliente atualizado com sucesso.")
-      resolve(resposta)
-    })
-  })
+        return new Promise((resolve, reject) => {
+            connection.query(sql, [clienteAtualizado, id], (error, resposta) => {
+
+                if (error) {
+                    console.log("Erro ao atualizar cliente");
+                    console.log(error.message);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Cliente atualizado com sucesso.");
+                resolve(resposta);
+            });
+        });
+    }
+
+    deletar(id) {
+        const sql = "DELETE FROM clientes WHERE id = ?";
+
+        return new Promise((resolve, reject) => {
+            connection.query(sql, [id], (error, resposta) => {
+
+                if (error) {
+                    console.log("Erro ao deletar o cliente");
+                    console.log(error.message);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Cliente deletado com sucesso.");
+                resolve(resposta);
+            });
+        });
+    }
 }
-deletar(id){
-  const sql = "DELETE FROM clientes WHERE id=?"
 
-  return new Promise((resolve, reject) => {
-    connection.query(sql, [id], (error, resposta) => {
-      if(error){
-        console.log("Erro ao deletar o cliente.")
-        console.log(error.message)
-        return reject(error)
-      }
-      console.log("Cliente deletado com sucesso.")
-      resolve(resposta)
-    })
-  })
-}
-}
-
-module.exports = new clienteModel()
+module.exports = new ClienteModel();
